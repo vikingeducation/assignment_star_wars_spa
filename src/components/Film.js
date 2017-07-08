cc1a78e7c3e16b1525c3e9c9f0eda3b497645d01
Christian Florez <florez.christian@gmail.com>
2017-07-08 19:16:08 -0400

    add specific film styling

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {Grid, Row, Col} from 'react-bootstrap';
 const Film = ({film}) => {
   return (
-    <div>
-      {JSON.stringify(film)}
-    </div>
+    <Grid>
+      <h1>Film: {film.title}</h1>
+      <Row>
+        <Col md={12}>
+          <h3>Director: {film.director}</h3>
+          <h3>Producer: {film.producer}</h3>
+          <br />
+          <h4>Released On: {film.release_date}</h4>
+          <br />
+          <p>
+            <strong>Opening Crawl: </strong>
+            {film.opening_crawl}
+          </p>
+        </Col>
+      </Row>
+    </Grid>
   );
 };
 
