4615f1088768b2d470d15b07b4b7bed850c18e6e
Christian Florez <florez.christian@gmail.com>
2017-07-08 14:59:18 -0400

    add initial films actions

---
+++ src/actions.js
@@ -0,0 +1,45 @@
+export const GET_FILMS_REQUEST = "GET_FILMS_REQUEST";
+export const GET_FILMS_SUCCESS = "GET_FILMS_SUCCESS";
+export const GET_FILMS_FAILURE = "GET_FILMS_FAILURE";
+const BASE_URI = 'http://swapi.co/api';
+
+export function getFilmsRequest() {
+  return {
+    type: GET_FILMS_REQUEST
+  };
+}
+
+export function getFilmsSuccess(data) {
+  return {
+    type: GET_FILMS_SUCCESS,
+    data
+  };
+}
+
+export function getFilmsFailure(error) {
+  return {
+    type: GET_FILMS_FAILURE,
+    error
+  };
+}
+
+export function getFilmsFromAPI() {
+  return dispatch => {
+    dispatch(getFilmsRequest());
+
+    fetch(`${BASE_URI}/films`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+
+        return response.json();
+      })
+      .then(json => {
+        dispatch(getFilmsSuccess(json));
+      })
+      .catch(error => {
+        dispatch(getFilmsFailure(error));
+      });
+  };
+}
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,88 @@
 import * as Actions from "./actions";
 import { combineReducers } from "redux";
 
-export const starWarsApp = combineReducers({});
+const initialState = {
+  films: {},
+  people: {},
+  planets: {},
+  species: {},
+  starships: {},
+  vehicles: {}
+}
+
+export function allFilms(state = initialState.films, action) {
+  switch (action.type) {
+    case Actions.GET_FILMS_SUCCESS:
+      return {
+        ...state,
+        allFilms: action.data,
+        isFetching: false,
+      }
+    case Actions.GET_FILMS_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+        error: null,
+      }
+    case Actions.GET_FILMS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    default:
+      return state
+  }
+}
+
+/* 
+
+  films, people, planets, species, starships, and vehicles
+  paginated
+  details-view
+
+  so routes are
+  / -> simple landing page
+  /films
+  /people
+  /planets
+  ... etc.
+
+  /films/:id
+  /people/:id
+  /planets/:id
+  ... etc.
+
+  We need to store into state:
+  -> allFilms
+    -> api response
+    -> current page
+  -> allPeople
+  -> allPlanets
+
+  -> selectedFilm
+  -> selectedPeople
+  -> selectedPlanet
+
+  So tomorrow, very simple:
+  First, set up working routes for all the resources + nav links, etc.
+  Then, get just ONE resource working perfectly. You get one, you have them all.
+
+  state = {
+    allFilms: {
+      films: api response
+      currentPage: Integer
+    }
+    selectedFilm: api response <- look at demo repo for clues; also goodreads assignment
+  }
+
+  Tricky parts 
+    -> What to do about pagination when jumping between resources? Reset?
+    -> Search? <- Should be under resources
+    -> Bonus Search: One on the landing page with an option-select for the resource
+
+*/
+
+export const starWarsApp = combineReducers({
+  allFilms
+});
