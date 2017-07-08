df625bfdc8f91697a7cba2a70dab0c75d0132e75
Christian Florez <florez.christian@gmail.com>
2017-07-08 03:45:03 -0400

    add redux thunk boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import 'bootswatch/flatly/bootstrap.min.css';
+
+import { starWarsApp } from "./reducers";
 import App from './components/App';
+import './index.css';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  starWarsApp,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/reducers.js
@@ -0,0 +1,4 @@
+import * as Actions from "./actions";
+import { combineReducers } from "redux";
+
+export const starWarsApp = combineReducers({});
