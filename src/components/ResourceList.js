5d82327c6d0591528d5431c6821c9613de032d52
Will Timpson <will.timpson@gmail.com>
2017-09-07 13:28:05 -0400

    Display those resources

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Panel } from "react-bootstrap";
 
-const ResourceList = props => {
-  console.log(props);
-  return (
-    <div>
-      <h2>ResourceList</h2>
-      <p>This is where the list goes</p>
-    </div>
-  );
+const Film = info => (
+  <Panel key={info.title} header={<h3>{info.title}</h3>}>
+    <p>{info.opening_crawl}</p>
+  </Panel>
+);
+
+const Person = info => (
+  <Panel key={info.name} header={<h3>{info.name}</h3>}>
+    <p>Born: {info.birth_year}</p>
+  </Panel>
+);
+
+const resourceMap = {
+  people: Person,
+  films: Film
 };
+
+const ResourceList = ({ resource, list }) => (
+  <div>{list.map(res => resourceMap[resource](res))}</div>
+);
 
 export default ResourceList;
