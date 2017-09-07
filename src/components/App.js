8aef70bdd3a56b34cec82a39fc3d9fd754fb73b7
Stephanie Barker <song_mage@yahoo.com>
2017-09-07 18:58:48 -0400

    merge conflict

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,13 @@ const App = () => (
       <Route exact path="/people" component={ListResourceContainer} />
       <Route exact path="/people/:id" component={ShowResourceContainer} />
       <Route exact path="/planets" component={ListResourceContainer} />
-      <Route exact path="/planets/:id" component={ShowPlanetContainer} />
+      <Route exact path="/planets/:id" component={ShowResourceContainer} />
       <Route exact path="/films" component={ListResourceContainer} />
-      <Route exact path="/films/:id" component={ShowFilmContainer} />
+      <Route exact path="/films/:id" component={ShowResourceContainer} />
       <Route exact path="/starships" component={ListResourceContainer} />
-      <Route exact path="/starships/:id" component={ShowStarshipContainer} />
+      <Route exact path="/starships/:id" component={ShowResourceContainer} />
       <Route exact path="/vehicles" component={ListResourceContainer} />
-      <Route exact path="/vehicles/:id" component={ShowVehicleContainer} />
+      <Route exact path="/vehicles/:id" component={ShowResourceContainer} />
     </div>
   </Router>
 );
