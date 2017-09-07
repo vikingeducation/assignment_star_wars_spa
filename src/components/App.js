10631716a8bdc251966e2bc0f5e820c484afd664
Will Timpson <will.timpson@gmail.com>
2017-09-07 11:17:18 -0400

    Boiled Plates

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const App = () => (
+  <div className="container">
+    <h2>Welcome!</h2>
+    <Button>Click Me!</Button>
+  </div>
+);
+
+export default App;
