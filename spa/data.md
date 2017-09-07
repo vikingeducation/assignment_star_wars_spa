d91893116f5651713472ae48f8f84d4bc3fa4311
Eric Glover <etgt28@mail.missouri.edu>
2017-09-07 10:24:34 -0500

    installations, thoughts, ui

---
+++ spa/data.md
@@ -0,0 +1,24 @@
+thoughts on things
+
+
+
+First set up a basic router with at least 2 different routes, using React Router.
+Decide how you want to implement state management and outline the structure. Before implementing it.
+One by one, add pages for each resource. Figure out how to work with the paginated data in the view. Add pagination. Don't forget to include links to each in either a navbar or sidebar.
+Each item on the main resource page should have a link to click to see the detailed information on that item, like a "show" view in a CRUD application.
+Add the ability to search.
+
+
+
+resources:
+https://swapi.co/api/
+species & starships
+http https://swapi.co/api/starships/9/
+  "results": [ {}, {} ]
+http https://swapi.co/api/species/3/
+
+Redux Store:
+{
+  starships,
+  species
+}
---
+++ spa/src/Components/elements/Link.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Link = props => {
+  const { name } = props;
+
+  return (
+    <div>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-toggle="modal"
+        data-target="#exampleModal"
+      >
+        {name}
+      </button>
+
+      <div
+        class="modal fade"
+        id="myModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="ModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              {/*
+           <button type="button" class="btn btn-primary">Save changes</button>
+          */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Link;
