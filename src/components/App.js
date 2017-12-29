9fcb4bec5c3456a297251c208caac59fd38c7161
Aaron Saloff <aaronsaloff@gmail.com>
2017-12-29 15:10:53 -0500

    set up routes

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <Router>
+      <div className="row">
+        <Nav />
+        <div className="MainContent col-10">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/films" render={() => <h1 className="text-center">FILMS</h1>} />
+            <Route exact path="/people" render={() => <h1 className="text-center">PEOPLE</h1>} />
+            <Route exact path="/starships" render={() => <h1 className="text-center">STARSHIPS</h1>} />
+            <Route exact path="/vehicles" render={() => <h1 className="text-center">VEHICLES</h1>} />
+            <Route exact path="/species" render={() => <h1 className="text-center">SPECIES</h1>} />
+            <Route exact path="/planets" render={() => <h1 className="text-center">PLANETS</h1>} />
+            <Route render={() => <h1 className="text-center">404 - Page Not Found</h1>} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './nav.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
